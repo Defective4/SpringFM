2f8cbe030b295c539454bc57d8b666beaac4d713
Defective <def3ctive4@gmail.com>
2025-07-22 22:33:53 +0200

    Initial backend structure

---
+++ backend/src/main/java/io/github/defective4/springfm/backend/profile/RadioProfile.java
@@ -0,0 +1,65 @@
+package io.github.defective4.springfm.backend.profile;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import io.github.defective4.springfm.server.packet.Packet;
+import io.github.defective4.springfm.server.packet.PacketGenerator;
+import io.github.defective4.springfm.server.service.RadioService;
+
+public class RadioProfile {
+    private final List<OutputStream> connectedClients = new ArrayList<>();
+    private int currentService = -1;
+    private final List<RadioService> services;
+
+    public RadioProfile(List<RadioService> services) {
+        this.services = Collections.unmodifiableList(services);
+        for (RadioService svc : services) svc.setPacketGenerator(new PacketGenerator() {
+            List<OutputStream> toRemove = new ArrayList<>();
+
+            @Override
+            public void packetGenerated(Packet packet) {
+                toRemove.clear();
+                if (packet.getType() == Packet.TYPE_AUDIO) {
+                    synchronized (connectedClients) {
+                        for (OutputStream os : connectedClients) {
+                            try {
+                                os.write(packet.getPayload());
+                            } catch (Exception e) {
+                                toRemove.add(os);
+                            }
+                        }
+                    }
+                }
+                synchronized (connectedClients) {
+                    toRemove.forEach(connectedClients::remove);
+                }
+            }
+        });
+    }
+
+    public void addClient(OutputStream os) {
+        synchronized (connectedClients) {
+            connectedClients.add(os);
+        }
+    }
+
+    public void setActiveService(int index) throws IOException {
+        if (index < -1) throw new IllegalArgumentException("Profile index can't be less than -1");
+        if (index >= services.size()) throw new IllegalArgumentException("Profile index is too big");
+        services.forEach(t -> {
+            try {
+                if (t.isStarted()) t.stop();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        if (index >= 0) {
+            services.get(index).start();
+        }
+        currentService = index;
+    }
+}
--- server/src/main/java/io/github/defective4/springfm/server/service/RadioService.java
+++ server/src/main/java/io/github/defective4/springfm/server/service/RadioService.java
@@ -2,9 +2,13 @@ package io.github.defective4.springfm.server.service;
 
 import java.io.IOException;
 
+import io.github.defective4.springfm.server.packet.PacketGenerator;
+
 public interface RadioService {
 
     boolean isStarted();
+
+    void setPacketGenerator(PacketGenerator generator);
 
     void start() throws IOException;
 
