08bb297d9552e352b21e83184b2a14acc168a3aa
Defective <def3ctive4@gmail.com>
2025-07-27 00:22:30 +0200

    Broadcast fm receiver

--- backend/src/main/java/io/github/defective4/springfm/backend/config/RadioConfiguration.java
+++ backend/src/main/java/io/github/defective4/springfm/backend/config/RadioConfiguration.java
@@ -1,6 +1,5 @@
 package io.github.defective4.springfm.backend.config;
 
-import java.io.File;
 import java.util.List;
 import java.util.Map;
 
@@ -10,8 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.github.defective4.springfm.backend.profile.RadioProfile;
-import io.github.defective4.springfm.server.service.impl.TestAnalogService;
-import io.github.defective4.springfm.server.service.impl.TestDigitalService;
+import io.github.defective4.springfm.server.service.impl.BroadcastFMService;
 
 @Configuration
 public class RadioConfiguration {
@@ -19,8 +17,7 @@ public class RadioConfiguration {
     @Bean
     Map<String, RadioProfile> getAvailableProfiles() {
         RadioProfile def = new RadioProfile(
-                List.of(new TestDigitalService(new File[] { new File("wbfm.wav"), new File("wbfm2.wav") }, "RMF FM"),
-                        new TestAnalogService(new File("wbfm2.wav"), "Jedynka")),
+                List.of(new BroadcastFMService("Broadcast FM", 87e6f, 108e6f, "rtl_tcp=127.0.0.1:55555", true, -1)),
                 new AudioFormat(171e3f, 16, 1, true, false));
         return Map.of("default", def);
     }
