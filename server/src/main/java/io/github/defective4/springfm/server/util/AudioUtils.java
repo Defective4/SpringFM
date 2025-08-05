2aac7e02afaf1922dea2bbdff541e8374dd61505
Defective <def3ctive4@gmail.com>
2025-08-05 20:33:23 +0200

    Creating WAV header with maximum length fields for compatibility

--- server/src/main/java/io/github/defective4/springfm/server/util/AudioUtils.java
+++ server/src/main/java/io/github/defective4/springfm/server/util/AudioUtils.java
@@ -3,6 +3,7 @@ package io.github.defective4.springfm.server.util;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 
 import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
@@ -17,6 +18,11 @@ public class AudioUtils {
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         AudioSystem.write(new AudioInputStream(InputStream.nullInputStream(), fmt, 0), AudioFileFormat.Type.WAVE,
                 buffer);
-        return buffer.toByteArray();
+        byte[] data = buffer.toByteArray();
+        byte[] intData = new byte[4];
+        Arrays.fill(intData, Byte.MAX_VALUE);
+        System.arraycopy(intData, 0, data, 4, intData.length);
+        System.arraycopy(intData, 0, data, data.length - 4, intData.length);
+        return data;
     }
 }
