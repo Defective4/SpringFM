67cf88a8a15c6972a34151b1a7153e922e5429c4
Defective <def3ctive4@gmail.com>
2025-08-07 17:10:09 +0200

    Force little-endian byte order

--- backend/src/main/java/io/github/defective4/springfm/backend/config/file/AudioFormatConfiguration.java
+++ backend/src/main/java/io/github/defective4/springfm/backend/config/file/AudioFormatConfiguration.java
@@ -3,15 +3,13 @@ package io.github.defective4.springfm.backend.config.file;
 import javax.sound.sampled.AudioFormat;
 
 public class AudioFormatConfiguration {
-    private final boolean bigEndian;
     private final float sampleRate;
     private final int sampleSize, channels;
 
-    public AudioFormatConfiguration(float sampleRate, int sampleSize, int channels, boolean bigEndian) {
+    public AudioFormatConfiguration(float sampleRate, int sampleSize, int channels) {
         this.sampleRate = sampleRate;
         this.sampleSize = sampleSize;
         this.channels = channels;
-        this.bigEndian = bigEndian;
     }
 
     public int getChannels() {
@@ -26,17 +24,13 @@ public class AudioFormatConfiguration {
         return sampleSize;
     }
 
-    public boolean isBigEndian() {
-        return bigEndian;
-    }
-
     public AudioFormat toAudioFormat() {
-        return new AudioFormat(sampleRate, sampleSize, channels, true, bigEndian);
+        return new AudioFormat(sampleRate, sampleSize, channels, true, false);
     }
 
     @Override
     public String toString() {
         return "AudioFormatConfiguration [sampleRate=" + sampleRate + ", sampleSize=" + sampleSize + ", channels="
-                + channels + ", bigEndian=" + bigEndian + "]";
+                + channels + "]";
     }
 }
