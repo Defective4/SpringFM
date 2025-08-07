3a8a496145f41bd3ba6337e29f83d830e598901c
Defective <def3ctive4@gmail.com>
2025-08-07 16:32:52 +0200

    Don't write custom format header at the beginning of audio stream

--- commons/src/main/java/io/github/defective4/springfm/server/data/SerializableAudioFormat.java
+++ commons/src/main/java/io/github/defective4/springfm/server/data/SerializableAudioFormat.java
@@ -1,26 +1,8 @@
 package io.github.defective4.springfm.server.data;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-
 import javax.sound.sampled.AudioFormat;
 
 public class SerializableAudioFormat {
-
-    public static class Codec {
-        public static SerializableAudioFormat fromStream(DataInputStream stream) throws IOException {
-            return new SerializableAudioFormat(stream.readFloat(), stream.readByte(), stream.readByte(),
-                    stream.readBoolean());
-        }
-
-        public static void toStream(SerializableAudioFormat format, DataOutputStream stream) throws IOException {
-            stream.writeFloat(format.sampleRate);
-            stream.writeByte(format.sampleSize);
-            stream.writeByte(format.channels);
-            stream.writeBoolean(format.bigEndian);
-        }
-    }
 
     private final boolean bigEndian;
     private final int channels;
