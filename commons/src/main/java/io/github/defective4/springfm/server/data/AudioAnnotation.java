e3e9af5fd0b6f0d96bc6f7a054d613c8a7c1072a
Defective <def3ctive4@gmail.com>
2025-08-08 23:45:35 +0200

    Added `nonMusic` field to audio annotations to detect non-music segments

--- commons/src/main/java/io/github/defective4/springfm/server/data/AudioAnnotation.java
+++ commons/src/main/java/io/github/defective4/springfm/server/data/AudioAnnotation.java
@@ -3,17 +3,19 @@ package io.github.defective4.springfm.server.data;
 import java.util.Objects;
 
 public class AudioAnnotation {
+    private final boolean nonMusic;
     private final String title, description;
 
-    public AudioAnnotation(String title, String description) {
+    public AudioAnnotation(String title, String description, boolean nonMusic) {
         this.title = title;
         this.description = description;
+        this.nonMusic = nonMusic;
     }
 
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
-        if ((obj == null) || (getClass() != obj.getClass())) return false;
+        if (obj == null || getClass() != obj.getClass()) return false;
         AudioAnnotation other = (AudioAnnotation) obj;
         return Objects.equals(description, other.description) && Objects.equals(title, other.title);
     }
@@ -31,9 +33,13 @@ public class AudioAnnotation {
         return Objects.hash(description, title);
     }
 
+    public boolean isNonMusic() {
+        return nonMusic;
+    }
+
     @Override
     public String toString() {
-        return "AudioAnnotation [title=" + title + ", description=" + description + "]";
+        return "AudioAnnotation [nonMusic=" + nonMusic + ", title=" + title + ", description=" + description + "]";
     }
 
 }
