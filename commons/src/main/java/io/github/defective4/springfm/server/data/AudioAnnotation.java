d23f3ad5a759708b535e119a33251915062b7ad5
Defective <def3ctive4@gmail.com>
2025-08-08 21:08:49 +0200

    Add `equals` method to audio annotation

--- commons/src/main/java/io/github/defective4/springfm/server/data/AudioAnnotation.java
+++ commons/src/main/java/io/github/defective4/springfm/server/data/AudioAnnotation.java
@@ -1,5 +1,7 @@
 package io.github.defective4.springfm.server.data;
 
+import java.util.Objects;
+
 public class AudioAnnotation {
     private final String title, description;
 
@@ -8,12 +10,25 @@ public class AudioAnnotation {
         this.description = description;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if ((obj == null) || (getClass() != obj.getClass())) return false;
+        AudioAnnotation other = (AudioAnnotation) obj;
+        return Objects.equals(description, other.description) && Objects.equals(title, other.title);
+    }
+
     public String getDescription() {
         return description;
     }
 
     public String getTitle() {
         return title;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(description, title);
     }
 
     @Override
